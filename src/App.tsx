c7d2f40bf2f4e915e78adbad7d89cfe1a9ce6815
Fitri <fitrirozi1@gmail.com>
2021-04-03 15:33:59 -0500

    put yarn.lock in .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
-function App(): JSX.Element {
+interface IResponse {
+  foo: string;
+  input: string;
+}
+
+function App() {
+  const [response, setResponse] = useState<IResponse>();
+
   useEffect(() => {
-    console.log("hello world");
+    axios.all([axios("/foo"), axios("/bar/3")]).then(([res1, res2]) => {
+      console.log(res1, res2);
+      setResponse({ foo: res1.data.foo, input: res2.data.input });
+    });
   }, []);
 
   return (
     <div className="app">
-      <h3>Hello</h3>
+      <div>
+        <h1>{response?.foo}</h1>
+        <h1>{response?.input}</h1>
+      </div>
     </div>
   );
 }
